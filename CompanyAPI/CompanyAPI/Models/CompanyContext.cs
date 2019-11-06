03cb2266ce744be9a7987f54f27dd624efb7e363
golovkinV <vladimir.golovkin16@yandex.ru>
2019-11-07 01:03:11 +0500

    add services controller

---
+++ CompanyAPI/CompanyAPI/Models/CompanyContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace CompanyAPI.Models
+{
+    public class CompanyContext: DbContext
+    {
+        public DbSet<Service> Services { get; set; }
+
+        public CompanyContext() {
+            this.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+        }
+    }
+}
