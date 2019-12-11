3bc86338a0249570dfe9e9f90ef3d4368436f74e
alls72 <aleksandr7211@gmail.com>
2019-12-11 23:37:26 +0500

    удаление, отмена заказа

--- CleanAndCode/public/js/API/API.js
+++ CleanAndCode/public/js/API/API.js
@@ -12,28 +12,38 @@ function sendRequest(type, url, data) {
         request.addEventListener("error", function() {
             reject(new Error("Network error"));
         });
-        type === 'GET' ? request.send() : request.send(data);
+        switch(type) {
+            case 'GET':
+                request.send();
+                break;
+            case 'POST':
+                request.send(data);
+                break;
+            case 'DELETE':
+                request.send();
+            case 'PUT':
+                request.send(data);
+        }
     });
 }
 
 
 function insertTableRow(order) {
+
+    const condition = order.ConditionId === 1 ? `Отменен` : `Отменить заказ`;
+
     let row = '<tr> ' +
         '<td class="font-weight-bold">' + `${order.Address}, ${order.Service.Name}` + '</td>' +
         '<td class="font-weight-bold">' + `${order.Date}, ${order.Time}` + '</td>' +
         '<td class="font-weight-bold">' + `${order.Condition.Name}` + '</td>' +
         '<td>' +
-            '<a href="/edit/">' +
-                '<button class="btn btn-warning">Отменить заказ' +
+                `<button class="btn btn-${order.ConditionId === 1 ? `secondary` : `warning` }" onclick="editOrder(${order.Id})" style="width: 100%">${condition}</button>` +
                 '</button>' +
-            '</a>' +
         '</td>' +
         '<td>' +
-            '<a href="/remove/">' +
                 `<button class="btn btn-warning" onclick="removeOrder(${order.Id})">` +
                     'Удалить заказ' +
                 '</button>' +
-            '</a>'
         '</td>' +
     '</tr>';
     return row;
